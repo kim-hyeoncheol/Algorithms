b265e1d2a39d28198fbc7134a63c94aa8ddcb1c1
Hyeon Cheol Kim <virspace@gmail.com>
2015-04-04 18:56:53 +0900

    Accept 8393 - Sum

---
+++ www.acmicpc.net/8393/Main.cpp
@@ -0,0 +1,17 @@
+/*
+ * 8393 - Sum
+ * https://www.acmicpc.net/problem/8393
+ * Run Time : 0 MS
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int n;
+    cin >> n;
+    cout << n * (n + 1) / 2 << endl;
+    return 0;
+}
+
+
