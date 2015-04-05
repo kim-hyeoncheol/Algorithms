a1ee343e0f9f122355a6e1e6d10d44ac27b14395
Hyeon Cheol Kim <virspace@gmail.com>
2015-04-05 15:39:35 +0900

    템플릿 스크립트 예외 처리
    argument 없으면, usage 를 보여준다.

--- www.acmicpc.net/make_template.sh
+++ www.acmicpc.net/make_template.sh
@@ -2,7 +2,10 @@
 
 PROBLEM_ID=$1
 
-mkdir ${PROBLEM_ID}
-cp template.Main.cpp ${PROBLEM_ID}/Main.cpp
-cp template.Makefile.cpp ${PROBLEM_ID}/Makefile
-
+if [ "${PROBLEM_ID}" != "" ]; then
+    mkdir ${PROBLEM_ID}
+    cp template.Main.cpp ${PROBLEM_ID}/Main.cpp
+    cp template.Makefile.cpp ${PROBLEM_ID}/Makefile
+else
+    echo "$0 id"
+fi
