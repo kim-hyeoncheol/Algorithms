e40e4b13e8fe6b7a276df5bc79b3e7fce1c86381
Hyeon Cheol Kim <virspace@gmail.com>
2015-04-09 12:45:10 +0900

    Accept - 1287 : 계산기 + BigInteger
    solution : 사칙연산 계산기(stack) + BigInteger
    Accept Code : Main.java (BigInteger 클래스 이용)
    Wrong Answer : main.cpp (BigInteger 구현이 없음)
    - 최초 cpp 로 계산기를 구현했지만, java 의 BigInteger 클래스를 이용하기 위해 java 코드로 포팅함.

--- www.acmicpc.net/1287/Main.java
+++ www.acmicpc.net/1287/Main.java
@@ -1,208 +1,156 @@
+/**
+ * 1287 - 할 수 있다
+ * https://www.acmicpc.net/problem/1287
+ * Run Time - 56 MS
+ * 사칙연산 계산기 (Stack) + Big Integer
+ */
 import java.util.Stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
-	private static final	char	PLUS = '+';
-	private static final	char	MINUS = '-';
-	
-	private static final	char	MUL = '*';
-	private static final	char	DIV = '/';
-	private static final	char	MOD = '%';
-	
-	private static final	char	POW = '^';
-	
-	private static final	char	FACTORIAL = '!';
 
-	private static final	char	LEFT = '(';
-	private static final	char	RIGHT = ')';
-	
-	private Boolean isValid;
-	private	String	infix;
-	private int     result;
+    private Boolean isValid;
+    private	String	infix;
+    private BigInteger  result;
 
-	Stack <Integer>		num = new Stack <Integer>();
-	Stack <Character>	op =  new Stack <Character>();
-	
-	Main (String str) {
-	    isValid = true;
-		infix = str.trim().replace(" ", "");
-		result = evaluate();
-	}
+    Stack <BigInteger>	num = new Stack <BigInteger>();
+    Stack <Character>	op =  new Stack <Character>();
 
-	public Boolean isValid() {
-	    return isValid;
+    Main (String str) {
+        infix = str;
+        isValid = isBalanced();
+        if (isValid)    evaluate();
     }
 
-    public int  getResult() {
+    public Boolean isValid() {
+        return isValid;
+    }
+
+    public Boolean isBalanced() {
+        Stack <Character>    paren = new Stack <Character>();
+        for (int i = 0; i < infix.length(); i++) {
+            if (infix.charAt(i) == '(')    paren.push('(');
+            else if (infix.charAt(i) == ')') {
+                if (paren.empty()) return false;
+                paren.pop();
+            }   
+        }   
+        return paren.empty();
+    }   // isBalanced()
+
+    public BigInteger getResult() {
         return result;
     }
-	
-	private int getPriority(char ch) {
-		switch (ch) {
-			case PLUS:	// +
-			case MINUS:	// -
-				return 1;
-			case MUL:	// *
-			case DIV:	// /
-			case MOD:	// %
-				return 2;
-			case POW:	// ^
-				return 3;
-			case FACTORIAL:	// !
-				return 5;
-			default:
-				return 0;
-		}
-	}
-	
-	private void evaluateStackTops() {
-		if (op.peek() == LEFT) {
-			return;
-		}
-		
-		if (num.size() < 2 || op.size() < 1) {
-			//while (!num.isEmpty()) {
-			//	num.pop();
-			//}
-			while (!op.isEmpty()) {
-				op.pop();
-			}
-			isValid = false;
-			return;
-		}
-		
-		int	operand2 = num.pop();
-		int	operand1 = num.pop();
-		
-		switch (op.pop()) {
-			case PLUS:
-				num.push(operand1 + operand2);
-				break;
-			case MINUS:
-				num.push(operand1 - operand2);
-				break;
-			case MUL:
-				num.push(operand1 * operand2);
-				break;
-			case DIV:
-				num.push(operand1 / operand2);
-				break;
-			case MOD:
-				num.push(operand1 % operand2);
-				break;
-			case POW:
-				int tmp = 1;
-				for (int i = 0; i < operand2; i++)
-					tmp *= operand1;
-				num.push(tmp);
-				break;
-		}
-	}
-	
-	private int evaluate() {
-		
-		char	ch;
-		int		sign = 1;
-		int		cur = 0;
-		boolean	isNum = false;
-		
-		for (int i = 0; i < infix.length(); i++) {
-			
-			ch = infix.charAt(i);
-			
-			switch (ch) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-					isNum = true;
-					cur = cur * 10 + ch -'0';
-					break;
-				case LEFT:
-					if (isNum) {
-						num.push(sign * cur);
-						isNum = false;
-						sign = 1;
-						cur = 0;
-					}
-					op.push(ch);
-					break;
-				case RIGHT:
-					if (isNum) {
-						num.push(sign * cur);
-						isNum = false;
-						sign = 1;
-						cur = 0;
-					}
-					while (op.peek() != LEFT) {
-						evaluateStackTops();
-					}
-					op.pop();
-					break;
-				case PLUS:
-				case MINUS:
-				case MUL:
-				case DIV:
-				case POW:
-				case MOD:
-					if (isNum) {
-						num.push(sign * cur);
-						isNum = false;
-						sign = 1;
-						cur = 0;
-					}
-					if (!op.isEmpty() && getPriority(op.peek()) >= getPriority(ch)) {
-						evaluateStackTops();
-					}
-					op.push(ch);
-					break;
-			}
-		}	// for
-		
-		if (isNum) {
-			num.push(sign * cur);
-		}
-		
-		while (!op.isEmpty()) {
-			evaluateStackTops();
-		}
-		
-		if (num.isEmpty()) {
-		    isValid = false;
-			return 0;
-		} else {
-			return num.pop();
-		}
-	}
+
+    private int getPriority(char ch) {
+        switch (ch) {
+            case '+':	// +
+            case '-':	// -
+                return 1;
+            case '*':	// *
+            case '/':	// /
+                return 2;
+            default:
+                return 0;
+        }
+    }   // getPriority()
+
+    private void evaluateStackTops() {
+        if (num.size() < 2 || op.size() < 1) {
+            isValid = false;
+            return;
+        }
+
+        if (op.peek() == '(') return;
+
+        BigInteger rh = num.pop();
+        BigInteger lh = num.pop();
+
+        switch (op.pop()) {
+            case '+':
+                num.push(lh.add(rh));
+                break;
+            case '-':
+                num.push(lh.subtract(rh));
+                break;
+            case '*':
+                num.push(lh.multiply(rh));
+                break;
+            case '/':
+                if (rh.equals(BigInteger.ZERO))   isValid = false;
+                else num.push(lh.divide(rh));
+                break;
+        }   // switch
+    }   // evaluateStackTops()
+
+    private void evaluate() {
+        boolean	isPrevNum = false;
+        char	ch;
+        int     i, j;
+
+        for (i = 0; isValid && i < infix.length(); i++) {
+            ch = infix.charAt(i);
+            if (Character.isDigit(ch)) {
+                for (j = i + 1; j < infix.length(); j++) {
+                    if (!Character.isDigit(infix.charAt(j))) break;
+                }
+
+                num.push(new BigInteger(infix.substring(i, j)));
+                i = j - 1;
+                isPrevNum = true;
+            } else {
+                switch (ch) {
+                    case '(':
+                        if (isPrevNum) isValid = false;
+                        op.push(ch);
+                        break;
+                    case ')':
+                        if (!isPrevNum) isValid = false;
+                        while (isValid && !op.empty() && op.peek() != '(')
+                            evaluateStackTops();
+                        if (!op.empty() && op.peek() != '(')   isValid = false;
+                        op.pop();
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                        if (isValid && !op.isEmpty() && getPriority(op.peek()) >= getPriority(ch))
+                            evaluateStackTops();
+                        op.push(ch);
+                        break;
+                    default:
+                        isValid = false;
+                        break;
+                }   // switch
+                isPrevNum = false;
+            }
+        }	// for
+
+        while (isValid && !op.isEmpty()) evaluateStackTops();
+
+        if (!op.isEmpty() || num.size() != 1) isValid = false;
+        else result = num.pop();
+    }
 
     public static void main(String[] args) {
         try {
             InputStreamReader   in = new InputStreamReader(System.in);
             BufferedReader  input = new BufferedReader(in);
-
             String  str;
-
             while ((str = input.readLine()) != null) {
                 if (str.length() == 0)  break;
 
                 Main  cal = new Main(str);
-                if (cal.isValid()) {
-                    System.out.println(cal.getResult());
-                } else {
-                    System.out.println("ROCK");
-                }
+                if (cal.isValid()) System.out.println(cal.getResult());
+                else System.out.println("ROCK");
             }   
         } catch (IOException e) {
             e.printStackTrace();
         }   
-    }   
+    }   // main()   
 }
 
---
+++ www.acmicpc.net/1287/main.cpp
@@ -0,0 +1,179 @@
+/*
+ * 2750 - 
+ * https://www.acmicpc.net/problem/2750
+ * Run Time : 
+ */
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+stack<long long>  num;
+stack<char> op;
+long long   result;
+string  infix;
+
+void debug_print() {
+    cout << "stack num" << endl;
+    while (!num.empty()) {
+        cout << num.top() << endl;
+        num.pop();
+    }
+    cout << "stack op" << endl;
+    while (!op.empty()) {
+        cout << op.top() << endl;
+        op.pop();
+    }
+}
+
+bool is_balanced() {
+    stack<char> paren;
+    for (int i = 0; i < infix.size(); i++) {
+        if (infix[i] == '(')    paren.push('(');
+        else if (infix[i] == ')') {
+            if (paren.empty()) return false;
+            paren.pop();
+        }
+    }
+    return paren.empty();
+}
+
+bool expr_top() {
+    int lh, rh;
+    if (op.size() < 1 || num.size() < 2)    return false;
+    rh = num.top();
+    num.pop();
+    lh = num.top();
+    num.pop();
+
+    //cout << "expr_top : " << lh << op.top() << rh << endl;
+
+    switch (op.top()) {
+        case '+':
+            num.push(lh + rh);
+            break;
+        case '-':
+            num.push(lh - rh);
+            break;
+        case '*':
+            num.push(lh * rh);
+            break;
+        case '/':
+            if (rh == 0)    return false;
+            num.push(lh / rh);
+            break;
+        default:
+            return false;
+    }
+    op.pop();
+    return true;
+}
+
+int get_priority(char ch) {
+    switch (ch) {
+        case '+':   return 1;
+        case '-':   return 1;
+        case '*':   return 2;
+        case '/':   return 2;
+        case '(':   return 0;
+    }
+    return 0;
+}
+
+bool is_numeric(char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+bool expr() {
+    if (!is_balanced()) return false;
+
+    int cur = 0;
+    bool    is_prev_num = false;
+    char    ch;
+
+    for (int i = 0; i < infix.size(); i++) {
+        ch = infix[i];
+        //cout << ch << endl;
+        if (is_numeric(ch)) {
+            if (is_prev_num && cur == 0)    return false;
+            cur = cur * 10 + infix[i] - '0';
+            is_prev_num = true;
+        } else {
+            if (is_prev_num)    num.push(cur);
+
+            switch (ch) {
+                case '(':
+                    if (is_prev_num) return false;
+                    op.push(ch);
+                    break;
+                case ')':
+                    if (!is_prev_num) return false;
+                    while (!op.empty() && op.top() != '(') {
+                        if (!expr_top())    return false;
+                    }
+                    if (op.top() != '(')    return false;
+                    op.pop();
+                    break;
+                case '+':
+                    if (!op.empty() && get_priority(op.top()) >= get_priority(ch)) {
+                        if (!expr_top())    return false;
+                    }
+                    op.push(ch);
+                    break;
+                case '-':
+                    if (!op.empty() && get_priority(op.top()) >= get_priority(ch)) {
+                        if (!expr_top())    return false;
+                    }
+                    op.push(ch);
+                    break;
+                case '*':
+                    if (!op.empty() && get_priority(op.top()) >= get_priority(ch)) {
+                        if (!expr_top())    return false;
+                    }
+                    op.push(ch);
+                    break;
+                case '/':
+                    if (!op.empty() && get_priority(op.top()) >= get_priority(ch)) {
+                        if (!expr_top())    return false;
+                    }
+                    op.push(ch);
+                    break;
+                default:
+                    return false;
+            }
+
+            cur = 0;
+            is_prev_num = false;
+        }
+        //if (!num.empty())
+            //cout << "cur top = " << num.top() << endl;
+    }   // for
+    
+    if (is_prev_num)    num.push(cur);
+    while (!op.empty()) {
+        if (!expr_top())    return false;
+    }
+    //debug_print();
+    if (!op.empty() || num.size() != 1) return false;
+    result = num.top();
+    num.pop();
+    return true;
+}   // expr();
+
+void final() {
+    while (!op.empty()) op.pop();
+    while (!num.empty())    num.pop();
+}
+
+int main(int argc, char* argv[]) {
+    while (cin >> infix) {
+        //cout << infix << " = " << endl;
+        if (expr()) cout << result << endl;
+        else    cout << "ROCK" << endl;
+
+        final();
+    }
+    return 0;
+}
+
