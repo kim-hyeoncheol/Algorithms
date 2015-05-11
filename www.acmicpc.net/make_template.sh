ee62e273f28fa10d65f5ac54d66f0358cffab73a
migom <virspace@gmail.com>
2015-05-11 10:09:36 +0900

    디렉토리가 이미 존재할 경우에도 덮어쓰는 문제 해결. 디렉토리가 이미 존재하면 메시지를 출력하고 종료한다.

--- www.acmicpc.net/make_template.sh
+++ www.acmicpc.net/make_template.sh
@@ -4,8 +4,12 @@ PROBLEM_ID=$1
 
 if [ "${PROBLEM_ID}" != "" ]; then
     mkdir ${PROBLEM_ID}
-    cp template.main.cpp ${PROBLEM_ID}/main.cpp
-    cp template.Makefile.cpp ${PROBLEM_ID}/Makefile
+    if [ $? -eq 0 ]; then
+        cp template.main.cpp ${PROBLEM_ID}/main.cpp
+        cp template.Makefile.cpp ${PROBLEM_ID}/Makefile
+    else
+        echo "${PROBLEM_ID} already exists.";
+    fi
 else
     echo "$0 id"
 fi
